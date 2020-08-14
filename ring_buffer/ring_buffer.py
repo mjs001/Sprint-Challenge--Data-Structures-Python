918d28d06a79464f9975af4544f83f9bbc37c874
mjs001 <maljayd42@gmail.com>
2020-08-14 10:37:28 -0500

    added in everthing and checked tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.storage = []
+        self.full = False
     def append(self, item):
-        pass
-
+        if self.full:
+            self.storage[self.oldest_index] = item
+            self.oldest_index = (self.oldest_index+1) % self.capacity
+        else:
+            self.storage.append(item)
+            if len(self.storage) == self.capacity:
+                self.full = True
+                self.oldest_index = 0
     def get(self):
-        pass
+        return self.storage
+
+    #setting variables and starting with false which makes it go to the else,
+    #adding to the storage and if the length of the storage matches capacity it sets to true and start the oldest index first
+    #and then it moves through the index and checks to see if its moved through all the way
